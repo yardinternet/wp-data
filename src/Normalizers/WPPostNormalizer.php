803afc82080a073a8dd246e6c18b91e2772ca430
Simon <simon@yard.nl>
2024-04-30 16:32:10 +0200

    (fix): don't run filters when constructing postData

---
+++ src/Normalizers/WPPostNormalizer.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yard\Data\Normalizers;
+
+use Spatie\LaravelData\Normalizers\Normalizer;
+
+class WPPostNormalizer implements Normalizer
+{
+    public function normalize(mixed $value): ?array
+    {
+        if (! $value instanceof \WP_Post) {
+            return null;
+        }
+
+        return $value->to_array();
+    }
+}
