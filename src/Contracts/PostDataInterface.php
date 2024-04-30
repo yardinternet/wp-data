61413ef7ce397d0b4a94a6040589a5b6e41ef68a
Simon <simon@yard.nl>
2024-04-30 17:19:43 +0200

    (feat): PostDataInterface

---
+++ src/Contracts/PostDataInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yard\Data\Contracts;
+
+use Carbon\CarbonImmutable;
+use Yard\Data\Enums\PostStatus;
+use Yard\Data\UserData;
+
+interface PostDataInterface
+{
+    public function __construct(
+        int $id,
+        ?UserData $author,
+        string $title,
+        string $content,
+        string $excerpt,
+        PostStatus $status,
+        CarbonImmutable $date,
+        CarbonImmutable $modified,
+        string $type,
+        string $slug,
+    );
+
+    public function id(): int;
+    public function author(): ?UserData;
+    public function title(): string;
+    public function content(): string;
+    public function excerpt(): string;
+    public function status(): string;
+    public function date(string $format): string;
+    public function modified(string $format): string;
+    public function type(): string;
+    public function slug(): string;
+    public function thumbnail(): string;
+}
