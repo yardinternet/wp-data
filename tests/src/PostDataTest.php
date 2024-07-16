1c821f2599340cf81d35a2cde0ceb5cbf36ef448
Simon <simon@yard.nl>
2024-07-16 11:48:18 +0200

    (feat): first PostData tests

---
+++ tests/src/PostDataTest.php
@@ -0,0 +1,39 @@
+<?php
+
+beforeEach(function () {
+    $this->postData = new Yard\Data\PostData(
+        id: 1,
+        author: null,
+        title: 'Hello, World!',
+        content: 'This is a test post.',
+        excerpt: 'This is a test excerpt.',
+        status: Yard\Data\Enums\PostStatus::PUBLISH,
+        date: Carbon\CarbonImmutable::now(),
+        modified: Carbon\CarbonImmutable::now(),
+        postType: 'post',
+        slug: 'hello-world',
+        thumbnail: null,
+    );
+});
+
+it('can construct PostData', function () {
+    expect($this->postData)->toBeInstanceOf(Yard\Data\PostData::class);
+});
+
+it('can get the URL of the post', function () {
+    \WP_Mock::userFunction('get_permalink', [
+        'args' => [1],
+        'return' => 'https://example.com/hello-world',
+    ]);
+
+    expect($this->postData->url())->toBe('https://example.com/hello-world');
+});
+
+it('returns an empty string when url does not exist', function () {
+    \WP_Mock::userFunction('get_permalink', [
+        'args' => [1],
+        'return' => false,
+    ]);
+
+    expect($this->postData->url())->toBe('');
+});
